393914969ea8a69de9282188ab31644d9c8f3d66
Jaume Capdevila Pérez <jaumecapdevila93@gmail.com>
2016-02-16 15:38:49 +0100

    Se ha eliminado código comentado

--- src/MpwarFramework/Component/Routes/Router.php
+++ src/MpwarFramework/Component/Routes/Router.php
@@ -1,15 +1,11 @@
 <?php
 
 namespace MpwarFramework\Component\Routes;
-use MpwarFramework\Component\Routes\yamlFileReader;
 
+use MpwarFramework\Component\Routes\yamlFileReader;
 
 class Router
 {
-    /*private $routes = [
-        'get' => [],
-        'post' => []
-    ];*/
     private $pathToRoutesFile;
 
     public function __construct($pathToRoutesFile)
@@ -17,45 +13,18 @@ class Router
         $this->pathToRoutesFile = $pathToRoutesFile;
     }
 
-    /*public function addGetRoute($pattern, callable $handler)
-    {
-        $this->routes['get'][$pattern] = $handler;
-        return $this;
-    }
-
-    public function addPostRoute($pattern, callable $handler)
-    {
-        $this->routes['post'][$pattern] = $handler;
-        return $this;
-    }*/
-
     public function matchRoute(Request $request)
     {
         $yamlReader = new yamlFileReader($this->pathToRoutesFile);
         $routes = $yamlReader->readFile();
 
 
-        foreach($routes as $route) {
-            if($route["path"] == $request->getPath()) {
+        foreach ($routes as $route) {
+            if ($route["path"] == $request->getPath()) {
                 echo "Route found";
             }
         }
 
         return false;
-        /*$method = strtolower($request->getMethod());
-        if (!isset($this->routes[$method])) {
-            return false;
-        }
-
-        $path = $request->getPath();
-        foreach ($this->routes[$method] as $pattern => $handler) {
-            if ($pattern === $path) {
-                return $handler;
-            }
-        }
-
-        return false;*/
     }
-
-
 }
