0c35457c3a98bd949da4241a0a03b7c4943cdbb6
Jaume Capdevila Pérez <jaumecapdevila93@gmail.com>
2016-02-23 23:22:55 +0100

    Se ha eliminado el use del componente Request

--- src/MpwarFramework/Component/Controller/Controller.php
+++ src/MpwarFramework/Component/Controller/Controller.php
@@ -3,13 +3,12 @@
 namespace MpwarFramework\Component\Controller;
 
 use MpwarFramework\Component\Response\responseFactory;
-use MpwarFramework\Component\Routes\Request;
 use MpwarFramework\Component\Templating\FileLoader;
 
 abstract class Controller
 {
     public function renderFile($fileToRender,$params) {
         $fileLoader = new FileLoader($fileToRender);
-        $fileLoader->renderWithAdequateTemplate($params);
+       return $fileLoader->renderWithAdequateTemplate($params);
     }
 }
