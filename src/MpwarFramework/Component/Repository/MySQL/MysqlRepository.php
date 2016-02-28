b7998e6e0ebfb3ea4dc54518ff0a17345152148f
Jaume Capdevila Pérez <jaumecapdevila93@gmail.com>
2016-02-28 11:38:24 +0100

    Se ha creado un nuevo método que permite especificar los parámetros de conexión con la base de datos

--- src/MpwarFramework/Component/Repository/MySQL/MysqlRepository.php
+++ src/MpwarFramework/Component/Repository/MySQL/MysqlRepository.php
@@ -10,16 +10,30 @@ class MysqlRepository
     private $user;
     private $password;
 
-    public function __construct($host, $database, $user, $password)
+    public function __construct()
     {
+        $this->host = "localhost";
+        $this->database = "";
+        $this->user = "root";
+        $this->password = "";
+    }
+
+    public function setDBConnectionParameters(
+        $host,
+        $database,
+        $user,
+        $password
+    ) {
         $this->host = $host;
         $this->database = $database;
         $this->user = $user;
         $this->password = $password;
     }
 
-    public function exectueQuery($query, $values = null)
-    {
+    public function exectueQuery(
+        $query,
+        $values = null
+    ) {
         try {
             $pdo = new \PDO('mysql:host=' . $this->host . ';dbname=' . $this->database, $this->user, $this->password);
             $statement = $pdo->prepare($query);
@@ -33,7 +47,6 @@ class MysqlRepository
             }
             $statement->execute();
             $queryResult = $statement->fetchAll($pdo::FETCH_ASSOC);
-            var_dump($queryResult);
             return $queryResult;
 
         } catch (\PDOException $e) {
