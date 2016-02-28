7416d23253969c07159d34d7a349864ffeabae0d
Jaume Capdevila Pérez <jaumecapdevila93@gmail.com>
2016-02-28 11:41:42 +0100

    Ahora el componente readerFactory añade también la terminación al nombre del archivo una vez ha instanciado el lector correspondiente

--- src/MpwarFramework/Component/Routes/readerFactory.php
+++ src/MpwarFramework/Component/Routes/readerFactory.php
@@ -7,10 +7,10 @@ class readerFactory
     static function instantiateReader($type,$fileToRead) {
         switch ($type) {
             case 'yml':
-                    return new yamlFileReader($fileToRead);
+                    return new yamlFileReader($fileToRead.".yml");
                 break;
             case 'xml':
-                    return new xmlFileReader($fileToRead);
+                    return new xmlFileReader($fileToRead.".xml");
                 break;
             case 'php':
                     echo "PHP";
